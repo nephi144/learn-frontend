32003131b45a45d95052101a71ddd03e0ac0283f
nephi144 <nephi_simene@yahoo.com>
2026-06-03 01:33:31 -1000

    Added the assignment page with the props and components

--- src/pages/AssignmentsPage.jsx
+++ src/pages/AssignmentsPage.jsx
@@ -1,11 +1,57 @@
+import { useState } from "react"
+
 import Navbar from "../components/Navbar"
+import AssignmentCard from "../components/AssignmentCard"
+import CreateAssignmentForm from "../components/CreateAssignmentForm"
 
 function AssignmentsPage() {
+
+  const [assignments, setAssignments] = useState([
+    {
+      id: 1,
+      title: "30 Minute Walk",
+      description: "Walk outside for 30 minutes.",
+      assignedTo: "Student001",
+      dueDate: "2026-06-20",
+    },
+  ])
+
+  function addAssignment(newAssignment) {
+
+    setAssignments([
+      ...assignments,
+      {
+        ...newAssignment,
+        id: assignments.length + 1,
+      },
+    ])
+  }
+
   return (
-    <div>
+    <div
+      style={{
+        padding: "20px",
+        maxWidth: "900px",
+        margin: "0 auto",
+      }}
+    >
       <Navbar />
 
-      <h1>Assignments Page</h1>
+      <h1>Assignments</h1>
+
+      <CreateAssignmentForm
+        addAssignment={addAssignment}
+      />
+
+      {assignments.map((assignment) => (
+        <AssignmentCard
+          key={assignment.id}
+          title={assignment.title}
+          description={assignment.description}
+          assignedTo={assignment.assignedTo}
+          dueDate={assignment.dueDate}
+        />
+      ))}
     </div>
   )
 }
